ce9ad49b0bcb8ac052a34960647e3100d1f0acce
protosam <samigknighted@gmail.com>
2021-06-26 05:11:59 -0500

    connectivity issue fixed

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,13 +8,19 @@ fi
 
 if [ -f "${SHARE_DIR}/.init-sync" ]; then
     echo "${SHARE_DIR}/.init-sync exists, skipping sync"
+    exit 0
+fi
+
+if [ "${SERVICE_NAME}" == "" ]; then
+    echo "SERVICE_NAME must be set to the name of the headless service"
+    exit 1
 fi
 
 cd /
 
 
 
-ncat --recv-only "${unnumbered_hostname}-$(($ordinal-1))" ${SYNC_PORT} | openssl enc -aes-256-cbc -pbkdf2 -d -pass "pass:${SHARED_SECRET}" | tar xf -
+ncat --recv-only "${unnumbered_hostname}-$(($ordinal-1)).${SERVICE_NAME}" ${SYNC_PORT} | openssl enc -aes-256-cbc -pbkdf2 -d -pass "pass:${SHARED_SECRET}" | tar xf -
 
 touch "${SHARE_DIR}/.init-sync"
 
